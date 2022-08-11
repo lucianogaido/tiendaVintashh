a1d2cc4b7bf935712a84797e956738d7257f4f29
aguspeiretti <aguspeiretti@gmail.com>
2022-08-11 16:09:08 -0300

    cambios en la nav y el main

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuFlotante = document.getElementById("menuFlotante");
+const menuFlotanteBtn = document.getElementById("menu-flotante-btn");
+
+menuFlotanteBtn.addEventListener("click", () => {
+  menuFlotante.classList.toggle("mostrar");
+});
